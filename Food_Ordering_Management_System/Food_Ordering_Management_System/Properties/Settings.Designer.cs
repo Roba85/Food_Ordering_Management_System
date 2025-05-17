85cf077e3bc8084408b7546064da050fe3b504de
haidy73 <haidygamal496@gmail.com>
2025-05-17 18:30:15 +0300

    Add menu with search feature

--- Food_Ordering_Management_System/Food_Ordering_Management_System/Properties/Settings.Designer.cs
+++ Food_Ordering_Management_System/Food_Ordering_Management_System/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Food_Ordering_Management_System.Properties
-{
-
-
+namespace Food_Ordering_Management_System.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-78QNVUR1;Initial Catalog=Food_ordering_system;Integrated Secur" +
+            "ity=True;TrustServerCertificate=True")]
+        public string Food_ordering_systemConnectionString {
+            get {
+                return ((string)(this["Food_ordering_systemConnectionString"]));
             }
         }
     }
